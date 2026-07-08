b2d7b312b9750f38e135065d1f912dfeda996303
Andres Felipe Gomez Loaiza <afgomez1998@hotmail.com>
2026-07-08 13:26:02 -0500

    Sprint 5 - Estado dinámico con useState

--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -1,7 +1,11 @@
-function ProgressCard() {
+function ProgressCard({ xp }) {
+
+  const porcentaje = (xp / 300) * 100;
+
   return (
     <div className="bg-slate-800 rounded-xl p-6 border border-slate-700 shadow-lg">
-      <h2 className="text-2xl font-bold text-white">
+
+      <h2 className="text-2xl font-bold">
         Tu progreso
       </h2>
 
@@ -10,19 +14,28 @@ function ProgressCard() {
       </p>
 
       <div className="w-full bg-slate-700 rounded-full h-3 mt-4">
+
         <div
-          className="bg-green-500 h-3 rounded-full"
-          style={{ width: "40%" }}
+          className="bg-green-500 h-3 rounded-full transition-all duration-500"
+          style={{
+            width: `${porcentaje}%`,
+          }}
         ></div>
+
       </div>
 
       <p className="mt-3 text-slate-300">
-        120 / 300 XP
+
+        {xp} / 300 XP
+
       </p>
 
       <p className="mt-2 text-orange-400">
+
         🔥 Racha: 3 días
+
       </p>
+
     </div>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,48 @@
-import ProgressCard from "../components/ProgressCard";
+import { useState } from "react";
+
 import CourseCard from "../components/CourseCard";
+import ProgressCard from "../components/ProgressCard";
 
 function Dashboard() {
+
+  const [xp, setXp] = useState(120);
+
   return (
     <div className="flex-1 p-10">
-      <h1 className="text-4xl font-bold text-white">
+
+      <h1 className="text-4xl font-bold">
         Bienvenido Andrés 👋
       </h1>
 
-  <div className="mt-8">
-    <ProgressCard />
-</div>
-
       <p className="text-slate-400 mt-2">
-        Continúa aprendiendo Inteligencia Artificial y Automatización.
+        Continúa aprendiendo IA y Automatización.
       </p>
 
+      <div className="mt-6">
+
+        <h2 className="text-xl font-semibold">
+
+          XP Actual: {xp}
+
+        </h2>
+
+        <button
+          onClick={() => setXp(xp + 25)}
+          className="bg-green-600 hover:bg-green-700 px-5 py-2 rounded-lg mt-4 transition-colors"
+        >
+          Completar lección (+25 XP)
+        </button>
+
+      </div>
+
+      <div className="mt-8">
+
+        <ProgressCard xp={xp} />
+
+      </div>
+
       <div className="grid md:grid-cols-3 gap-6 mt-10">
+
         <CourseCard
           title="React desde cero"
           progress="10%"
@@ -31,7 +57,9 @@ function Dashboard() {
           title="Automatización con n8n"
           progress="0%"
         />
+
       </div>
+
     </div>
   );
 }
